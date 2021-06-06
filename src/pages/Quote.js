a18746397e6e116a889adb348f778ec7556fde35
harini <harinisiva1997@gmail.com>
2021-06-06 23:50:15 +0530

    nested routes

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,12 +1,16 @@
 import React from "react"
-import { useParams } from "react-router"
-
+import { Route, useParams } from "react-router"
+import Comments from '../components/comments/Comments'
 const Quote = props => {
     const params = useParams()
     const quoteId = params.quoteId
     return <React.Fragment>
         <h1>Quote page</h1>
         <p>{quoteId}</p>
+        {/* <Route path={`/quotes/${quoteId}/comments`}> */}
+        <Route path="/quotes/:quoteId/comments">
+            <Comments></Comments>
+        </Route>
     </React.Fragment>
 }
 export default Quote
