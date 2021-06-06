768e9d423be3932fa2e60ca512c98d76c26a080e
harini <harinisiva1997@gmail.com>
2021-06-07 00:11:38 +0530

    adding layout (navlinks)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import { Redirect, Route, Switch } from "react-router";
+import Layout from "./components/layout/Layout";
 import NewQuote from "./pages/NewQuote";
 import Quote from "./pages/Quote";
 import Quotes from "./pages/Quotes";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/quotes" />
-      </Route>
-      <Route path="/quotes" exact>
-        <Quotes />
-      </Route>
-      <Route path="/quotes/:quoteId">
-        <Quote />
-      </Route>
-      <Route path="/new-quotes">
-        <NewQuote />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <Quotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <Quote />
+        </Route>
+        <Route path="/new-quotes">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
