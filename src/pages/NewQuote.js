c4e1cf5ce02ecaf6468acc4d831950bef3223502
harini <harinisiva1997@gmail.com>
2021-06-07 23:01:49 +0530

    progrmatic navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,11 @@
+import { useHistory } from "react-router"
 import QuoteForm from "../components/quotes/QuoteForm"
 
 const NewQuote = props => {
+    const history = useHistory()
     const addQuoteHandler = QuoteData => {
         console.log(QuoteData)
+        history.push("/quotes")
     }
     return <QuoteForm onAddQuote={addQuoteHandler} />
 }
